446bb3ad08a5816823cee168eec3113296190437
Alder <aldersanchez163@gmail.com>
2025-01-10 13:13:42 +0100

    creando REST/Controller

--- src/factories/EnvioFactory.java
+++ src/factories/EnvioFactory.java
@@ -12,7 +12,7 @@ public class EnvioFactory {
     // Rename the variable to avoid conflict with the Java keyword `package`
     private static EnvioManager envioManager;
 
-    public static EnvioManager getPackageInstance() {
+    public static EnvioManager getEnvioInstance() {
         if (envioManager == null) {
             envioManager = (EnvioManager) new EnvioRESTClient();
         }
